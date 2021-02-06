81a61dcee93064032b3f3cd2de6702d2115a03d0
Greenfin17 <jmaple@yahoo.com>
2021-02-05 23:32:09 -0600

    more styling, adding photo

--- projects.js
+++ projects.js
@@ -1,9 +1,21 @@
 console.log("CONNECTED");
 
+
 const projects = [
+  {
+    title: "Sorting Hat",
+    screenshot: "screenshots/SortingHat.png",
+    altText: "Screen shot of Sorting Hat project",
+    description: "Mimics the Sorting Hat fo the Harry Potter series",
+    technologiesUsed: "HTML, CSS, Javascript, Github for version control",
+    available: true,
+    url : "https://jm-hogwarts-sorting-hat.netlify.app",
+    githubUrl : "https://github.com/Greenfin17/sorting-hat",
+  },
   {
     title: "Pet Adoption",
     screenshot: "screenshots/PetAdoption.png",
+    altText: "Screen shot of Pet Adoption project",
     description: "Creates a complex rythm metronome",
     technologiesUsed: "HTML, CSS, Javascript, Github for version control",
     available: true,
@@ -13,20 +25,12 @@ const projects = [
   {
     title: "Ski Shop",
     screenshot: "screenshots/SkiShop.png",
+    altText: "Screen shot of Ski Shop product cards",
     description: "Creates a complex rythm metronome",
     technologiesUsed: "HTML, CSS, Javascript, Github for version control",
     available: true,
     url : "https://jm-product-cards.netlify.app",
     githubUrl : "https://github.com/Greenfin17/product-cards",
-  },
-  {
-    title: "Cool Project3",
-    screenshot: "screenshot3.jpg",
-    description: "Creates a complex rythm metronome",
-    technologiesUsed: "HTML, CSS, Javascript, Github for version control",
-    available: false,
-    url : "https://github.com/Greenfin17/samplesite",
-    githubUrl : "https://github.com/Greenfin17/samplesite",
   },
 ]  
 
@@ -45,7 +49,8 @@ const createProjectCards = (projectArr) => {
       domString += `<div class="card my-2" style="width: 18rem;" id=${i}>
         <div class="card-body">
           <p class="card-text project-title">${item.title}</p>
-          <div class="img-container" style="background-image: url('${item.screenshot}');"></div>
+          <div class="img-container" style="background-image: url('${item.screenshot}');" 
+          alt="${item.altText}"></div>
           <p class="card-text technologies">Technologies: ${item.technologiesUsed}</p>
           <p class="card-text url"><a href="${item.url}">Site Link</a></p>
           <p class="card-text githubUrl"><a href="${item.githubUrl}">GitHub</a></p>
